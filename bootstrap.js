10366818354a9d013c68a8a2306c7b159607f26f
Infocatcher <infocatcher.dev.null@gmail.com>
2013-04-10 21:55:39 +0400

    Make filter async, draft for #2 (not fully tested!)

--- bootstrap.js
+++ bootstrap.js
@@ -947,129 +947,181 @@ EventHandler.prototype = {
 			this.updateHintDelay();
 
 		regularFilter && !noStats && this.showFilter(true /*ignoreNotFound*/);
-		this.filterBookmarksPopup(popup, filterString, matcher, false, popup); //~ todo: "linear" pref ?
-		regularFilter && !noStats && this.showFilter();
+		if(this._filterInProgress)
+			this._filterCallback = this.filterBookmarksPopup.bind(this, popup, filterString, matcher, false, popup);
+		else
+			this.filterBookmarksPopup(popup, filterString, matcher, false, popup);
+		//regularFilter && !noStats && this.showFilter();
 	},
-	filterBookmarksPopup: function(popup, filterString, matcher, linear, parentPopup, _level) {
-		var firstCall = _level === undefined; // First internal call
-		if(firstCall) {
-			_level = 1;
-			this._lastCount = 0;
-			cancelTimer(this._ignoreActivationTimer);
-			this._ignoreActivation = true;
-		}
-		else if(++_level > prefs.get("filterMaxLevel", 5)) {
-			_log("filterMaxLevel reached, stop filtering");
-			return true;
-		}
-
-		var childs = Array.slice(popup.childNodes);
-		var hasVisible = false;
-
-		childs.forEach(function(node) {
-			var hide = false;
-			if(this.isBookmark(node)) {
-				if(matcher) {
-					var text = this.getBookmarkText(node);
-					if(text && !matcher(text))
-						hide = true;
-				}
-				if(!hide) {
-					hasVisible = true;
-					++this._lastCount;
-					//if(
-					//	++this._lastCount % 10 == 0
-					//	&& popup == this._filterPopup
-					//)
-					//	this.updateFilterCount();
-				}
-				//~ todo: implement linear mode ?
+	_filterInProgress: false,
+	_filterCallback: null,
+	_filterAsyncTimer: 0,
+	filterBookmarksPopup: function() {
+		this._filterInProgress = true;
+		function worker(popup, filterString, matcher, linear, parentPopup, callback, _level) {
+			var firstCall = _level === undefined; // First internal call
+			if(firstCall) {
+				_level = 1;
+				this._lastCount = 0;
+				cancelTimer(this._ignoreActivationTimer);
+				this._ignoreActivation = true;
 			}
-			else if(node.localName == "menu" && this.isBookmarkItem(node)) {
-				var mp = node.menupopup || node.getElementsByTagName("menupopup")[0];
-				var load = !mp.hasAttribute(this.attrLoaded)
-					|| node.getElementsByAttribute("query", "true").length > 0;
-				var prevCount = this._lastCount;
-
-				if(load) {
-					// We should simulate popupshowing to trigger built-in bookmarks loader
-					//_log("Load bookmarks tree");
-					mp[this.pIgnorePopup] = true;
-					node.collapsed = true;
-					node.open = true;
-				}
-				var subMatcher = matcher && prefs.get("checkFoldersLabels", true) && matcher(node.getAttribute("label"))
-					? null
-					: matcher;
-				if(!this.filterBookmarksPopup(mp, filterString, subMatcher, linear, parentPopup, _level))
-					hide = true;
-				else
-					hasVisible = true;
-				if(load) {
-					node.open = false;
-					node.collapsed = false;
-					delete mp[this.pIgnorePopup];
-
-					Array.forEach(
-						node.getElementsByTagName("menupopup"),
-						function(node) {
-							node.setAttribute(this.attrLoaded, "true");
-						},
-						this
-					);
-				}
-				mp[this.pFilter] = filterString;
-				mp[this.pCount] = this._lastCount - prevCount;
+			else if(++_level > prefs.get("filterMaxLevel", 5)) {
+				_log("filterMaxLevel reached, stop filtering");
+				//return true;
+				return;
 			}
 
-			if(hide)
-				this.hideNode(node);
-			else if(node.hasAttribute(this.attrHidden))
-				this.showNode(node);
-		}, this);
+			var childs = Array.slice(popup.childNodes);
+			var hasVisible = false;
+			var _this = this;
 
-		//if(firstCall) {
-		//	popup[this.pFilter] = filterString;
-		//	popup[this.pCount] = this._lastCount; // ?
-		//}
+			for(var i = 0, l = childs.length; i < l; ++i) {
+				var node = childs[i];
+				var hide = false;
+				if(this.isBookmark(node)) {
+					if(matcher) {
+						var text = this.getBookmarkText(node);
+						if(text && !matcher(text))
+							hide = true;
+					}
+					if(!hide) {
+						hasVisible = true;
+						++this._lastCount;
+						//if(
+						//	++this._lastCount % 10 == 0
+						//	&& popup == this._filterPopup
+						//)
+						//	this.updateFilterCount();
+					}
+					//~ todo: implement linear mode ?
+				}
+				else if(node.localName == "menu" && this.isBookmarkItem(node)) {
+					var mp = node.menupopup || node.getElementsByTagName("menupopup")[0];
+					var load = !mp.hasAttribute(this.attrLoaded)
+						|| node.getElementsByAttribute("query", "true").length > 0;
+					var prevCount = this._lastCount;
 
-		// Hide nested separators
-		var hasVisibleSibling = false;
-		function hideNestedSeparators(node) {
-			if(node.localName == "menuseparator") {
-				if(!hasVisibleSibling)
+					if(load) {
+						// We should simulate popupshowing to trigger built-in bookmarks loader
+						//_log("Load bookmarks tree");
+						mp[this.pIgnorePopup] = true;
+						node.collapsed = true;
+						node.open = true;
+					}
+					var subMatcher = matcher && prefs.get("checkFoldersLabels", true) && matcher(node.getAttribute("label"))
+						? null
+						: matcher;
+
+					//if(!this.filterBookmarksPopup(mp, filterString, subMatcher, linear, parentPopup, _level))
+					//	hide = true;
+					//else
+					//	hasVisible = true;
+					this._filterAsyncTimer = timer(function() {
+						if(this._filterCallback) {
+							_gen.next();
+							return;
+						}
+						this.filterBookmarksPopup(mp, filterString, subMatcher, linear, parentPopup, function(_hasVisible) {
+							if(!_hasVisible)
+								hide = true;
+							else
+								hasVisible = true;
+							_gen.next();
+						}, _level);
+					}, this);
+					yield;
+
+					if(load) {
+						node.open = false;
+						node.collapsed = false;
+						delete mp[this.pIgnorePopup];
+
+						Array.forEach(
+							node.getElementsByTagName("menupopup"),
+							function(node) {
+								node.setAttribute(this.attrLoaded, "true");
+							},
+							this
+						);
+					}
+					mp[this.pFilter] = filterString;
+					mp[this.pCount] = this._lastCount - prevCount;
+				}
+
+				if(hide)
 					this.hideNode(node);
-				else if(this.canBeVisible(node))
-					hasVisibleSibling = false;
+				else if(node.hasAttribute(this.attrHidden))
+					this.showNode(node);
 			}
-			else if(this.canBeVisible(node)) {
-				hasVisibleSibling = true;
-				return true;
+
+			//if(firstCall) {
+			//	popup[this.pFilter] = filterString;
+			//	popup[this.pCount] = this._lastCount; // ?
+			//}
+
+			// Hide nested separators
+			var hasVisibleSibling = false;
+			function hideNestedSeparators(node) {
+				if(node.localName == "menuseparator") {
+					if(!hasVisibleSibling)
+						this.hideNode(node);
+					else if(this.canBeVisible(node))
+						hasVisibleSibling = false;
+				}
+				else if(this.canBeVisible(node)) {
+					hasVisibleSibling = true;
+					return true;
+				}
+				return false;
 			}
-			return false;
+
+			childs.forEach(hideNestedSeparators, this);
+			hasVisibleSibling = false;
+			childs.reverse().some(hideNestedSeparators, this);
+
+			if(firstCall) {
+				cancelTimer(this._selectBMNodeTimer);
+				this._selectBMNodeTimer = timer(function() {
+					var lastActive = this._lastActiveNode;
+					if(lastActive && (lastActive.parentNode != popup || !this.isNodeVisible(lastActive)))
+						lastActive = null;
+					if(lastActive && this._activeNode == lastActive) // Nothing to do :)
+						return;
+					_log("restoreActiveItem: " + (lastActive ? lastActive.getAttribute("label") : "<first>"));
+					this.restoreActiveItem(popup, lastActive);
+				}, this, 0);
+				this._ignoreActivationTimer = timer(function() {
+					this._ignoreActivation = false;
+				}, this, 50);
+
+				this.stopFilterDelay();
+				this.showFilterDelay();
+
+				this._filterInProgress = false;
+				var filterCallback = this._filterCallback;
+				if(filterCallback) {
+					this._filterCallback = null;
+					filterCallback.call(this);
+				}
+			}
+			else {
+				this.showFilterDelay(true /*ignoreNotFound*/); // Ajust position
+			}
+
+			//return hasVisible;
+			callback && callback.call(this, hasVisible);
+			yield;
 		}
-
-		childs.forEach(hideNestedSeparators, this);
-		hasVisibleSibling = false;
-		childs.reverse().some(hideNestedSeparators, this);
-
-		if(firstCall) {
-			cancelTimer(this._selectBMNodeTimer);
-			this._selectBMNodeTimer = timer(function() {
-				var lastActive = this._lastActiveNode;
-				if(lastActive && (lastActive.parentNode != popup || !this.isNodeVisible(lastActive)))
-					lastActive = null;
-				if(lastActive && this._activeNode == lastActive) // Nothing to do :)
-					return;
-				_log("restoreActiveItem: " + (lastActive ? lastActive.getAttribute("label") : "<first>"));
-				this.restoreActiveItem(popup, lastActive);
-			}, this, 0);
-			this._ignoreActivationTimer = timer(function() {
-				this._ignoreActivation = false;
-			}, this, 50);
-		}
-
-		return hasVisible;
+		var _gen = worker.apply(this, arguments);
+		var _startTime = Date.now();
+		_gen.next();
+	},
+	stopFilter: function() {
+		cancelTimer(this._filterAsyncTimer);
+		this.stopFilterDelay();
+		this._filterInProgress = false;
+		this._filterCallback = null;
 	},
 	restoreActiveItem: function(popup, item) {
 		var document = popup.ownerDocument;
@@ -1178,6 +1230,22 @@ EventHandler.prototype = {
 		else
 			this.ttRemoveClass(clss);
 	},
+	_showFilterScheduled: false,
+	_showFilterDelayTimer: 0,
+	showFilterDelay: function() {
+		if(this._showFilterScheduled)
+			return;
+		this._showFilterScheduled = true;
+		var args = arguments;
+		this._showFilterDelayTimer = timer(function() {
+			this._showFilterScheduled = false;
+			this.showFilter.apply(this, args);
+		}, this, 70);
+	},
+	stopFilterDelay: function() {
+		cancelTimer(this._showFilterDelayTimer);
+		this._showFilterScheduled = false;
+	},
 	showFilter: function(ignoreNotFound, popup, s, _noRetry) {
 		popup = popup || this._currentPopup;
 		s = s || this._filter;
@@ -1187,8 +1255,10 @@ EventHandler.prototype = {
 		var count = popup[this.pCount] || 0;
 		tt._count.setAttribute("value", count);
 
-		if(!ignoreNotFound)
-			this.ttSetClass("bookmarksMenuFilter-notFound", !count && s);
+		var notFound = !count && s;
+		_log("!!! showFilter: count: " + count + " notFound: " + notFound + " ignore: " + ignoreNotFound);
+		if(!notFound || !ignoreNotFound)
+			this.ttSetClass("bookmarksMenuFilter-notFound", notFound);
 
 		var w = Math.max(prefs.get("minPanelWidth", 120), popup.boxObject.width);
 		var trgPopup = this._filterPopup;
@@ -1280,6 +1350,7 @@ EventHandler.prototype = {
 	hideFilter: function() {
 		_log("hideFilter()");
 		this.tt.realHidePopup();
+		this.stopFilter();
 	},
 	showFilterFlags: function(flags) {
 		var flagsStr = "";
