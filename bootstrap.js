b8c4448a8c2a32b5d2d8dffee9b62d0a384fba18
Infocatcher <infocatcher.dev.null@gmail.com>
2013-12-24 20:43:02 +0400

    Revert "Simplify nsITimer usage:" Doesn't work in Firefox 3.6 and older
    This reverts commit 483b69e98e907b91821091cbcbc020b75399d70c.

--- bootstrap.js
+++ bootstrap.js
@@ -1563,9 +1563,11 @@ function timer(callback, context, delay, args) {
 	var id = ++_timersCounter;
 	var timer = _timers[id] = Components.classes["@mozilla.org/timer;1"]
 		.createInstance(Components.interfaces.nsITimer);
-	timer.init(function() {
-		delete _timers[id];
-		callback.apply(context, args);
+	timer.init({
+		observe: function(subject, topic, data) {
+			delete _timers[id];
+			callback.apply(context, args);
+		}
 	}, delay || 0, timer.TYPE_ONE_SHOT);
 	return id;
 }
