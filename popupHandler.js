Infocatcher/Bookmarks_Menu_Filter

commit 0c2db8029b0b79a8caa9c27a87fbc480250e3b68

Correctly hide tooltip after double Alt in Firefox 3.6

popupHandler.js
@@ -1199,7 +1199,11 @@ PopupHandler.prototype = {
 	},
 	hideFilter: function() {
 		_log("hideFilter()");
-		this.tt.realHidePopup();
+		//this.tt.realHidePopup();
+		// May work wrong without delay in Firefox 3.6: Alt, Alt -> menus closes, but not tooltip
+		this.window.setTimeout(function(tt) {
+			tt.realHidePopup();
+		}, 0, this.tt);
 		this.stopFilter();
 	},
 	showFilterFlags: function(flags) {

commit 0cffa12fd3ed01eceb94d90efc616e1be46bb22c

Correctly cancel filter hiding (bug introduced in 0c2db8029b0b79a8caa9c27a87fbc480250e3b68 commit)

popupHandler.js
@@ -1110,6 +1110,11 @@ PopupHandler.prototype = {
 		var trgPopup = this._filterPopup;
 		this._filterPopup = popup;
 
+		if(this._hideFilterTimer) {
+			this.window.clearTimeout(this._hideFilterTimer);
+			this._hideFilterTimer = 0;
+		}
+
 		if(platformVersion < 2) {
 			if(
 				tt.boxObject.width != w
@@ -1200,13 +1205,15 @@ PopupHandler.prototype = {
 	updateFilterCount: function() {
 		this.tt._count.setAttribute("value", this._lastCount || 0);
 	},
+	_hideFilterTimer: 0,
 	hideFilter: function() {
 		_log("hideFilter()");
 		//this.tt.realHidePopup();
 		// May work wrong without delay in Firefox 3.6: Alt, Alt -> menus closes, but not tooltip
-		this.window.setTimeout(function(tt) {
-			tt.realHidePopup();
-		}, 0, this.tt);
+		this._hideFilterTimer = this.window.setTimeout(function(_this) {
+			_this._hideFilterTimer = 0;
+			_this.tt.realHidePopup();
+		}, 0, this);
 		this.stopFilter();
 	},
 	showFilterFlags: function(flags) {