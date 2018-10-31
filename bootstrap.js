cd2b3586ab0cc7ee02e3a96a6674586c9c65571e
Infocatcher <infocatcher.dev.null@gmail.com>
2018-10-31 16:14:49 +0300

    Move prefs handling into separate file + temporary debug logs (#10)

--- bootstrap.js
+++ bootstrap.js
@@ -14,6 +14,12 @@ this.__defineGetter__("PopupHandler", function() {
 	Services.scriptloader.loadSubScript(rootURI + "popupHandler.js", this, "UTF-8");
 	return PopupHandler;
 });
+this.__defineGetter__("prefs", function() {
+	delete this.prefs;
+	Services.console.logStringMessage("bmf: " + ts() + "load prefs.js\n" + new Error().stack);
+	Services.scriptloader.loadSubScript(rootURI + "prefs.js", this, "UTF-8");
+	return prefs;
+});
 
 function install(params, reason) {
 	try {
@@ -26,6 +32,7 @@ function install(params, reason) {
 function uninstall(params, reason) {
 }
 function startup(params, reason) {
+	Services.console.logStringMessage("bmf: " + ts() + "startup");
 	platformVersion = parseFloat(Services.appinfo.platformVersion);
 	if(Services.appinfo.name == "Pale Moon" || Services.appinfo.name == "Basilisk")
 		platformVersion = platformVersion >= 4.1 ? 56 : 28;
@@ -189,109 +196,6 @@ var bmFilter = {
 		var loc = window.location.href;
 		return loc == "chrome://browser/content/browser.xul"
 			|| loc == "chrome://navigator/content/navigator.xul";
-	}
-};
-
-var prefs = {
-	ns: "extensions.bookmarksMenuFilter.",
-	initialized: false,
-	init: function() {
-		if(this.initialized)
-			return;
-		this.initialized = true;
-
-		//~ todo: add new condition when https://bugzilla.mozilla.org/show_bug.cgi?id=564675 will be fixed
-		if(platformVersion >= 2)
-			this.loadDefaultPrefs();
-		Services.prefs.addObserver(this.ns, this, false);
-	},
-	delayedInit: function() {
-		if(!this.initialized)
-			timer(this.init, this, 100);
-	},
-	destroy: function() {
-		if(!this.initialized)
-			return;
-		this.initialized = false;
-
-		Services.prefs.removeObserver(this.ns, this);
-	},
-	observe: function(subject, topic, pName) {
-		if(topic != "nsPref:changed")
-			return;
-		var shortName = pName.substr(this.ns.length);
-		var pVal = this.getPref(pName);
-		this._cache[shortName] = pVal;
-		bmFilter.prefChanged(shortName, pVal);
-	},
-
-	loadDefaultPrefs: function() {
-		var defaultBranch = Services.prefs.getDefaultBranch("");
-		var prefsFile = rootURI + "defaults/preferences/prefs.js";
-		var prefs = this;
-		Services.scriptloader.loadSubScript(prefsFile, {
-			pref: function(pName, val) {
-				var pType = defaultBranch.getPrefType(pName);
-				if(pType != defaultBranch.PREF_INVALID && pType != prefs.getValueType(val)) {
-					Components.utils.reportError(
-						LOG_PREFIX + 'Changed preference type for "' + pName
-						+ '", old value will be lost!'
-					);
-					defaultBranch.deleteBranch(pName);
-				}
-				prefs.setPref(pName, val, defaultBranch);
-			}
-		});
-	},
-
-	_cache: { __proto__: null },
-	get: function(pName, defaultVal) {
-		var cache = this._cache;
-		return pName in cache
-			? cache[pName]
-			: (cache[pName] = this.getPref(this.ns + pName, defaultVal));
-	},
-	set: function(pName, val) {
-		this.setPref(this.ns + pName, val);
-	},
-	getPref: function(pName, defaultVal, prefBranch) {
-		var ps = prefBranch || Services.prefs;
-		switch(ps.getPrefType(pName)) {
-			case ps.PREF_BOOL:   return ps.getBoolPref(pName);
-			case ps.PREF_INT:    return ps.getIntPref(pName);
-			case ps.PREF_STRING:
-				if("getStringPref" in ps) // Firefox 58+
-					return ps.getStringPref(pName);
-				return ps.getComplexValue(pName, Components.interfaces.nsISupportsString).data;
-		}
-		return defaultVal;
-	},
-	setPref: function(pName, val, prefBranch) {
-		var ps = prefBranch || Services.prefs;
-		var pType = ps.getPrefType(pName);
-		if(pType == ps.PREF_INVALID)
-			pType = this.getValueType(val);
-		switch(pType) {
-			case ps.PREF_BOOL:   ps.setBoolPref(pName, val); break;
-			case ps.PREF_INT:    ps.setIntPref(pName, val);  break;
-			case ps.PREF_STRING:
-				if("setStringPref" in ps) { // Firefox 58+
-					ps.setStringPref(pName, val);
-					break;
-				}
-				var ss = Components.interfaces.nsISupportsString;
-				var str = Components.classes["@mozilla.org/supports-string;1"]
-					.createInstance(ss);
-				str.data = val;
-				ps.setComplexValue(pName, ss, str);
-		}
-	},
-	getValueType: function(val) {
-		switch(typeof val) {
-			case "boolean": return Services.prefs.PREF_BOOL;
-			case "number":  return Services.prefs.PREF_INT;
-		}
-		return Services.prefs.PREF_STRING;
 	}
 };
 
